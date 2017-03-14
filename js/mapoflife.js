9fe43a4aba50c8fae43ea41aded58f42db1e8ce9
James Rock <hello@jamesrock.me>
2017-03-14 21:26:20 +0000

    initial commit

---
+++ js/mapoflife.js
@@ -0,0 +1,25 @@
+(function() {
+
+	console.log("mapoflife");
+
+	var
+	places = {
+		map: {
+			LABEL: 0,
+			LAT: 1,
+			LONG: 2,
+			DATE: 3
+		},
+		data: [
+			["Primary School", 0, 0],
+			["Secondary School", 0, 0],
+			["Ali's House", 0, 0],
+			["Gran's House", 0, 0],
+			["Mom's House", 0, 0],
+			["Flat #1 in Nottingham", 0, 0],
+			["Flat #1 in London", 0, 0],
+			["Flat #2 in London", 0, 0]
+		]
+	};
+
+})();
