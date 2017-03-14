0e7a2eb17c7e432faa70e874868c05bd4ee31c4a
James Rock <hello@jamesrock.me>
2017-03-14 21:34:30 +0000

    more places

--- js/mapoflife.js
+++ js/mapoflife.js
@@ -18,7 +18,11 @@
 			["Mom's House", 0, 0],
 			["Flat #1 in Nottingham", 0, 0],
 			["Flat #1 in London", 0, 0],
-			["Flat #2 in London", 0, 0]
+			["Flat #2 in London", 0, 0],
+			["Saw Mascara Story", 0, 0], // derby venue of some sort
+			["Rock City", 0, 0], // saw Saw Mascara Story, InMe, +44
+			["Nottingham Ice Arena", 0, 0], // saw Green Day and Blink 182
+			["Download Festival 2005", 0, 0]
 		]
 	};
 
